e171d529533b0415a32df0fb03366e02c27b86ce
Jaeyong <koola97620@nate.com>
2019-07-08 00:25:31 +0900

    Objects Week2 Chapter04 start

---
+++ objects/src/main/java/Chapter04/Movie.java
@@ -0,0 +1,15 @@
+package Chapter04;
+
+import java.time.Duration;
+
+/**
+ * @author choijaeyong on 08/07/2019.
+ * @project objects
+ * @description
+ */
+public class Movie {
+  private String title;
+  private Duration runningTime;
+  
+
+}
