0d262442848c8898401661325368343204ed4856
Jaeyong <koola97620@nate.com>
2019-07-13 10:17:00 +0900

    Objects Week2 Chapter04 start

--- objects/src/main/java/Chapter04/Movie.java
+++ objects/src/main/java/Chapter04/Movie.java
@@ -1,15 +1,23 @@
-package Chapter04;
-
-import java.time.Duration;
-
-/**
- * @author choijaeyong on 08/07/2019.
- * @project objects
- * @description
- */
-public class Movie {
-  private String title;
-  private Duration runningTime;
-  
-
-}
+//package Chapter04;
+//
+//import Chapter02.DiscountCondition;
+//import java.time.Duration;
+//import java.util.List;
+//
+///**
+// * @author choijaeyong on 08/07/2019.
+// * @project objects
+// * @description
+// */
+//public class Movie {
+//  private String title;
+//  private Duration runningTime;
+//  private Money fee;
+//  private List<DiscountCondition> discountConditions;
+//
+//  private MovieType movieType;
+//  private Money discountAmount;
+//  private double discountPercent;
+//
+//
+//}
