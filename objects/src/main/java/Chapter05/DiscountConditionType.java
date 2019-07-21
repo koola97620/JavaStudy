d462a41cdb803278cbd2fc63642fb5ff8394f035
jdragon <jdragon@i-screamedu.co.kr>
2019-07-21 13:13:17 +0900

    Objects Chapter 05

---
+++ objects/src/main/java/Chapter05/DiscountConditionType.java
@@ -0,0 +1,11 @@
+package Chapter05;
+
+/**
+ * @author choijaeyong on 20/07/2019.
+ * @project objects
+ * @description
+ */
+public enum DiscountConditionType {
+  SEQUENCE,
+  PERIOD
+}
---
+++ objects/src/main/java/Chapter05/MovieType.java
@@ -0,0 +1,12 @@
+package Chapter05;
+
+/**
+ * @author choijaeyong on 20/07/2019.
+ * @project objects
+ * @description
+ */
+public enum MovieType {
+  AMOUNT_DISCOUNT,
+  PERCENT_DISCOUNT,
+  NONE_DISCOUNT
+}
