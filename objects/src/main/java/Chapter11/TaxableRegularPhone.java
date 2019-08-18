f708625663f1bf3117c7f2f91a927cb3cf30b4ee
Jaeyong <koola97620@nate.com>
2019-08-18 13:15:09 +0900

    Objects Chapter11

--- objects/src/main/java/Chapter11/TaxableRegularPhone.java
+++ objects/src/main/java/Chapter11/TaxableRegularPhone.java
@@ -21,4 +21,8 @@ public class TaxableRegularPhone extends RegularPhone{
     return fee.plus(fee.times(taxRate));
   }
 
+  @Override
+  protected Money afterCalculated(Money fee) {
+    return fee.plus(fee.times(taxRate));
+  }
 }
