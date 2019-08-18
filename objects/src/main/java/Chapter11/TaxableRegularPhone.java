6fbc99c5e616089a576dcdcb62e09dea20f5ae9b
Jaeyong <koola97620@nate.com>
2019-08-18 11:38:25 +0900

    Objects Chapter11

---
+++ objects/src/main/java/Chapter11/TaxableRegularPhone.java
@@ -0,0 +1,24 @@
+package Chapter11;
+
+import java.time.Duration;
+
+/**
+ * @author choijaeyong on 17/08/2019.
+ * @project objects
+ * @description
+ */
+public class TaxableRegularPhone extends RegularPhone{
+  private double taxRate;
+
+  public TaxableRegularPhone(Money amount, Duration seconds, double taxRate) {
+    super(amount,seconds);
+    this.taxRate = taxRate;
+  }
+
+  @Override
+  public Money calculateFee() {
+    Money fee = super.calculateFee();
+    return fee.plus(fee.times(taxRate));
+  }
+
+}
