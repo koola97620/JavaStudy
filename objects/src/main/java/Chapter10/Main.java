44f716975bcebdc29b1b5c45d33d788d5799fbb4
Jaeyong <koola97620@nate.com>
2019-08-23 23:47:42 +0900

    Objects Chapter12

--- objects/src/main/java/Chapter10/Main.java
+++ objects/src/main/java/Chapter10/Main.java
@@ -10,15 +10,15 @@ import java.time.LocalDateTime;
  */
 public class Main {
   public static void main(String[] args) {
-    Phone phone = new Phone(Money.wons(5l) , Duration.ofSeconds(10));
-    phone.call(new Call(LocalDateTime.of(2018,1,1,12,10,0) ,
-        LocalDateTime.of(2018,1,1,12,11,0)));
-    phone.call(new Call(LocalDateTime.of(2018,1,2,12,10,0) ,
-        LocalDateTime.of(2018,1,2,12,11,0)));
-
-    Money result = phone.calculateFee();
-
-    System.out.println(result.getMoney().intValue());
+//    Phone phone = new Phone(Money.wons(5l) , Duration.ofSeconds(10));
+//    phone.call(new Call(LocalDateTime.of(2018,1,1,12,10,0) ,
+//        LocalDateTime.of(2018,1,1,12,11,0)));
+//    phone.call(new Call(LocalDateTime.of(2018,1,2,12,10,0) ,
+//        LocalDateTime.of(2018,1,2,12,11,0)));
+//
+//    Money result = phone.calculateFee();
+//
+//    System.out.println(result.getMoney().intValue());
 
   }
 
