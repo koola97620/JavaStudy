bb10456b09fe192a8928efe44871513a942cdb56
Jaeyong <koola97620@nate.com>
2020-03-18 00:27:44 +0900

    item2

---
+++ effectivejava/src/test/java/chapter2/Pizza.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+import java.util.Set;
+
+/**
+ * @author choijaeyong on 2020/03/18.
+ * @project effectivejava
+ * @description
+ */
+public abstract class Pizza {
+  public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
+  Set<Topping> toppings;
+
+
+
+}
---
+++ effectivejava/src/test/java/chapter2/item2.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+/**
+ * @author choijaeyong on 2020/03/18.
+ * @project effectivejava
+ * @description
+ */
+public class item2 {
+
+}
