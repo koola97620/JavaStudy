c83ee69e0c4ded3a42439f26ace7008103244213
Jaeyong <koola97620@nate.com>
2020-04-12 22:38:28 +0900

    chapter5.5

--- java8action/src/test/java/chapter5/TransactionTest.java
+++ java8action/src/test/java/chapter5/TransactionTest.java
@@ -1,5 +1,8 @@
 package chapter5;
 
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,9 +12,30 @@ import org.junit.jupiter.api.Test;
  */
 public class TransactionTest {
 
-  @Test
+  private List<Transaction> transactions;
+
+  @BeforeEach
   public void setUp() {
+    Trader raoul = new Trader("Raoul", "Cambridge");
+    Trader mario = new Trader("Mario", "Cambridge");
+    Trader alan = new Trader("Alan", "Cambridge");
+    Trader brian = new Trader("Brian", "Cambridge");
+
+    transactions = Arrays.asList(
+        new Transaction(brian, 2011, 300),
+        new Transaction(raoul, 2012, 1000),
+        new Transaction(raoul, 2011, 400),
+        new Transaction(mario, 2012, 710),
+        new Transaction(mario, 2012, 700),
+        new Transaction(alan, 2012, 950)
+    );
+  }
+
+  @Test
+  public void test1() {
 
   }
+
+
 
 }
