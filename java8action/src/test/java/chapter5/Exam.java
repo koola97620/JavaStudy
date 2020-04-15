5756501466817d3a9d717a50b7aead410aff5214
Jaeyong <koola97620@nate.com>
2020-04-15 13:32:27 +0900

    chapter5

--- java8action/src/test/java/chapter5/Exam.java
+++ java8action/src/test/java/chapter5/Exam.java
@@ -1,6 +1,10 @@
 package chapter5;
 
 import chapter5.Dish.Type;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -137,10 +141,28 @@ public class Exam {
 
     Stream<String> stream = Stream.of("Java8 ", "Lamdas " , "In ", "Action");
     stream.map(String::toUpperCase).forEach(System.out::println);
+  }
 
+  @Test
+  public void page179() throws Exception {
+    long uniqueWords = 0;
+    try (Stream<String> lines =
+        Files.lines(Paths.get("data.txt") , Charset.defaultCharset() )) {
+      uniqueWords = lines.flatMap( line -> Arrays.stream(line.split(" ")))
+          .distinct()
+          .count();
+    } catch (IOException e) {
 
+    }
 
+    Stream.iterate(0, n -> n+2)
+        .limit(10)
+        .forEach(System.out::println);
 
+    Stream.iterate(new int[]{0,1} , t -> new int[]{t[1] , t[0] + t[1]})
+        .limit(20)
+        .map(t -> t[0])
+        .forEach(System.out::println);
 
   }
 
