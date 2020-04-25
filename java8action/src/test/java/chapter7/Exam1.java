a3b40d895f4e55f1c1345f4a038d9fe1f04ddced
Jaeyong <koola97620@nate.com>
2020-04-25 21:35:32 +0900

    chapter7

--- java8action/src/test/java/chapter7/Exam1.java
+++ java8action/src/test/java/chapter7/Exam1.java
@@ -1,5 +1,6 @@
 package chapter7;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +27,18 @@ public class Exam1 {
         .limit(n)
         .parallel()
         .reduce(0L, Long::sum);
+  }
+
+  public long measureSumPerf(Function<Long,Long> adder, long n) {
+    long fastest = Long.MAX_VALUE;
+    for (int i=0; i < 10 ; i++) {
+      long start = System.nanoTime();
+      long sum = adder.apply(n);
+      long duration = (System.nanoTime() - start) /1_000_000;
+      System.out.println("result : " + sum);
+      if (duration < fastest) fastest = duration;
+    }
+    return fastest;
   }
 
 }
