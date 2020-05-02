b80c11427cb0b17f80608ba179a741a4644d2c16
Jaeyong <koola97620@nate.com>
2020-05-02 20:02:16 +0900

    chapter8

---
+++ java8action/src/test/java/chapter8/HeaderTextProcessing.java
@@ -0,0 +1,14 @@
+package chapter8;
+
+/**
+ * @author choijaeyong on 2020/05/02.
+ * @project java8action
+ * @description
+ */
+public class HeaderTextProcessing extends ProcessingObject<String> {
+
+  @Override
+  protected String handleWork(String input) {
+    return null;
+  }
+}
