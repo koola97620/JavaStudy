f4588e7406c54c985d7affca1472ae79457db792
Jaeyong <koola97620@nate.com>
2020-05-17 20:39:02 +0900

    chapter10

--- java8action/src/test/java/chapter10/Exam.java
+++ java8action/src/test/java/chapter10/Exam.java
@@ -1,5 +1,6 @@
 package chapter10;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -11,36 +12,62 @@ public class Exam {
 
   @Test
   public void test() {
+    Person person = new Person();
+    Optional<Car> car = person.getCar();
+
+    Optional<Insurance> insurance = car.get().getInsurance();
+
+    String name = null;
+    if (insurance.isPresent()) {
+      name = insurance.get().getName();
+    }
+
+    Insurance insurance2 = car.get().getInsurance().get();
+    Optional<Insurance> optInsurance = Optional.ofNullable(insurance2);
+    Optional<String> s = optInsurance.map(Insurance::getName);
+  }
+
+  @Test
+  public void test2() {
+    Person person = new Person();
+    Optional<Person> optPerson = Optional.of(person);
+    Optional<String> s = optPerson.map(Person::getCar)
+        .map(c -> c.get().getInsurance())
+        .map(i -> i.get().getName());
+  }
+
+  @Test
+  public void test3() {
 
   }
 
-  private String getCarInsuraceName(Person person) {
-    if (person != null) {
-      Car car = person.getCar();
-      if (car != null) {
-        Insurance insurance = car.getInsurance();
-        if (insurance != null) {
-          return insurance.getName();
-        }
-      }
-    }
-    return "Unknown";
-  }
-
-  public String getCarInsuranceName(Person person) {
-    if (person == null) {
-      return "Unknown";
-    }
-
-    Car car = person.getCar();
-    if (car == null) {
-      return "Unknown";
-    }
-    Insurance insurance = car.getInsurance();
-    if (insurance == null) {
-      return "Unknown";
-    }
-    return insurance.getName();
-  }
+//  private String getCarInsuraceName(Person person) {
+//    if (person != null) {
+//      Car car = person.getCar();
+//      if (car != null) {
+//        Insurance insurance = car.getInsurance();
+//        if (insurance != null) {
+//          return insurance.getName();
+//        }
+//      }
+//    }
+//    return "Unknown";
+//  }
+//
+//  public String getCarInsuranceName(Person person) {
+//    if (person == null) {
+//      return "Unknown";
+//    }
+//
+//    Car car = person.getCar();
+//    if (car == null) {
+//      return "Unknown";
+//    }
+//    Insurance insurance = car.getInsurance();
+//    if (insurance == null) {
+//      return "Unknown";
+//    }
+//    return insurance.getName();
+//  }
 
 }
