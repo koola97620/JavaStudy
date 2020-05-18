244037e29506822618ac8deb64b63baa670a8cdd
Jaeyong <koola97620@nate.com>
2020-05-18 23:45:02 +0900

    chapter11

---
+++ java8action/src/test/java/chapter11/Shop.java
@@ -0,0 +1,11 @@
+package chapter11;
+
+/**
+ * @author choijaeyong on 2020/05/18.
+ * @project java8action
+ * @description
+ */
+public interface Shop {
+  public double getPrice(String product);
+
+}
