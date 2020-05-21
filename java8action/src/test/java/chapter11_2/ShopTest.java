ea162f9a3a6087012be4c31086915c7374e093ab
Jaeyong <koola97620@nate.com>
2020-05-22 00:31:07 +0900

    chapter11

--- java8action/src/test/java/chapter11_2/ShopTest.java
+++ java8action/src/test/java/chapter11_2/ShopTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import chapter11.ShopImpl;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -42,9 +43,19 @@ class ShopTest {
   }
 
   public List<String> findPricesParallel(String product) {
-    return shops.stream()
-        .map(shop -> String
-            .format("%s price is %.2f", shop.getName(), shop.getPrice(product)))
+    return shops.parallelStream()
+        .map(shop -> String.format("%s price is %.2f", shop.getName(), shop.getPrice(product)))
+        .collect(Collectors.toList());
+  }
+
+  public List<String> findPricesAsync(String product) {
+    List<CompletableFuture<String>> priceFutures = shops.stream()
+        .map(shop -> CompletableFuture.supplyAsync(
+            () -> shop.getName() + " price is " + shop.getPrice(product)
+        )).collect(Collectors.toList());
+
+    return priceFutures.stream()
+        .map(CompletableFuture::join)
         .collect(Collectors.toList());
   }
 
