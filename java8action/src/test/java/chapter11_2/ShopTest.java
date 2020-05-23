8a77b5ca8294872e20dda19f7bb27dece2d77a95
Jaeyong <koola97620@nate.com>
2020-05-23 19:15:24 +0900

    chapter11

--- java8action/src/test/java/chapter11_2/ShopTest.java
+++ java8action/src/test/java/chapter11_2/ShopTest.java
@@ -6,6 +6,9 @@ import chapter11.ShopImpl;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -58,5 +61,15 @@ class ShopTest {
         .map(CompletableFuture::join)
         .collect(Collectors.toList());
   }
+
+  private final Executor executor =
+      Executors.newFixedThreadPool(Math.min(shops.size(), 100), new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+          Thread t = new Thread(r);
+          t.setDaemon(true);
+          return t;
+        }
+      });
 
 }
