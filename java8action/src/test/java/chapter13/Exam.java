ed8bd1ddda2fe1163b924f3840b5046d5481b4a6
Jaeyong <koola97620@nate.com>
2020-05-29 21:35:01 +0900

    chapter13

--- java8action/src/test/java/chapter13/Exam.java
+++ java8action/src/test/java/chapter13/Exam.java
@@ -1,5 +1,8 @@
 package chapter13;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -30,5 +33,37 @@ public class Exam {
         .max(Comparator.comparing(Transaction::getValue));
 
   }
+
+  // 13.2.3
+  @Test
+  public void test2() {
+    List<Integer> integers = Arrays.asList(1, 4, 9);
+  }
+  public static List<List<Integer>> subsets(List<Integer> list) {
+    if (list.isEmpty()) {
+      List<List<Integer>> ans = new ArrayList<>();
+      ans.add(Collections.emptyList());
+      return ans;
+    }
+    Integer first = list.get(0);
+    List<Integer> rest = list.subList(1, list.size());
+
+    List<List<Integer>> subans = subsets(rest);
+    List<List<Integer>> subans2 = insertAll(first, subans);
+    return concat(subans,subans2);
+
+
+  }
+
+  private static List<List<Integer>> concat(List<List<Integer>> subans,
+      List<List<Integer>> subans2) {
+    return null;
+  }
+
+  private static List<List<Integer>> insertAll(Integer first, List<List<Integer>> subans) {
+
+    return null;
+  }
+
 
 }
