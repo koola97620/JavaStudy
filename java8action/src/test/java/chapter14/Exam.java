d00ee62f5de592207460487e8d5ad8e2412e9858
Jaeyong <koola97620@nate.com>
2020-05-31 20:02:26 +0900

    chapter14

---
+++ java8action/src/test/java/chapter14/Exam.java
@@ -0,0 +1,14 @@
+package chapter14;
+
+/**
+ * @author choijaeyong on 2020/05/31.
+ * @project java8action
+ * @description
+ */
+public class Exam {
+
+  static double converter(double x, double f, double b) {
+    return x * f + b;
+  }
+
+}
